81611bc9007a3a2e3b62dca8118eb5c29df69730
Raven <raven.aharon.2002@gmail.com>
2024-06-12 23:29:28 +0300

    Set nvim as git editor

--- install/user-install.zsh
+++ install/user-install.zsh
@@ -6,6 +6,7 @@ INSTALLER_DIR="${0:a:h}/.."
 source "$INSTALLER_DIR/zsh-utils.zsh"
 
 # *** Script *********************************************
+yes_no_prompt "set nvim as git editor?" && git config core.editor nvim
 yes_no_prompt "Install aur?" && "$INSTALLER_DIR/install/install-yay.zsh" && yay -S - < "$INSTALLER_DIR/pkglists/pkglist-aur"
 yes_no_prompt "configure nix?" && "$INSTALLER_DIR/setup-nix.zsh"
 yes_no_prompt "Install extra-nix-packages?" && "$INSTALLER_DIR/install-extra-nix-packages.zsh"

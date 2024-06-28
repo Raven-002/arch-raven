21d76ea860f7defeb5f1659e5b89dbc89aa9f00d
Raven <152022920+Raven-002@users.noreply.github.com>
2024-06-28 15:04:47 +0000

    Improve theming

--- install/install-breeze-dark.zsh
+++ install/install-breeze-dark.zsh
@@ -7,11 +7,41 @@ source "$INSTALLER_DIR/zsh-utils.zsh"
 
 # *** Script *********************************************
 mkdir -p ~/.config
-ln -srT "$INSTALLER_DIR/config/gtk/gtk-3.0" "$HOME/.config/gtk-3.0"
-ln -srT "$INSTALLER_DIR/config/gtk/gtk-4.0" "$HOME/.config/gtk-4.0"
-ln -srT "$INSTALLER_DIR/config/gtk/gtkrc" "$HOME/.config/gtkrc"
-ln -srT "$INSTALLER_DIR/config/gtk/gtkrc-2.0" "$HOME/.config/gtkrc-2.0"
+if yes_no_prompt "Configure gtk Breeze dark?"; then
+    ln -srT "$INSTALLER_DIR/config/gtk/gtk-3.0" "$HOME/.config/gtk-3.0"
+    ln -srT "$INSTALLER_DIR/config/gtk/gtk-4.0" "$HOME/.config/gtk-4.0"
+    ln -srT "$INSTALLER_DIR/config/gtk/gtkrc" "$HOME/.config/gtkrc"
+    ln -srT "$INSTALLER_DIR/config/gtk/gtkrc-2.0" "$HOME/.config/gtkrc-2.0"
+fi
 
-ln -srT "$INSTALLER_DIR/config/qt/kdeglobals" "$HOME/.config/kdeglobals"
-ln -srT "$INSTALLER_DIR/config/qt/qt5ct" "$HOME/.config/qt5ct"
-ln -srT "$INSTALLER_DIR/config/qt/qt6ct" "$HOME/.config/qt6ct"
+if yes_no_prompt "Configure qt breeze dark?"; then
+    ln -srT "$INSTALLER_DIR/config/qt/kdeglobals" "$HOME/.config/kdeglobals"
+    ln -srT "$INSTALLER_DIR/config/qt/qt5ct" "$HOME/.config/qt5ct"
+    ln -srT "$INSTALLER_DIR/config/qt/qt6ct" "$HOME/.config/qt6ct"
+fi
+
+if yes_no_prompt "Configure flatpak (qt does not work well)?"; then
+    flatpak override --user --filesystem=xdg-config/gtk-4.0:ro
+    flatpak override --user --filesystem=xdg-config/gtk-3.0:ro
+    flatpak override --user --filesystem=xdg-config/gtk-2.0:ro
+    flatpak override --user --filesystem=xdg-config/gtkrc-2.0:ro
+    flatpak override --user --filesystem=xdg-config/gtkrc:ro
+
+    flatpak override --user --filesystem=xdg-config/qt6ct:ro
+    flatpak override --user --filesystem=xdg-config/qt5ct:ro
+
+    # for gtk4 mostly
+    mkdir -p "$HOME/.themes"
+    cp -r "/usr/share/themes/Breeze-Dark" "$HOME/.themes"
+    flatpak override --user --filesystem=~/.themes:ro
+
+    flatpak override --user --env=GTK_THEME=Breeze-Dark
+    flatpak override --user --env=QT_QPA_PLATFORMTHEME=qt6ct
+    flatpak override --user --env=QT_QPA_PLATFORM=wayland
+    flatpak override --user --env=QT_STYLE_OVERRIDE=breeze
+fi
+
+if yes_no_prompt "Configure flatpak electron?"; then
+    flatpak override --user --env=ELECTRON_OZONE_PLATFORM_HINT=auto
+    flatpak override --user --socket=wayland
+fi
